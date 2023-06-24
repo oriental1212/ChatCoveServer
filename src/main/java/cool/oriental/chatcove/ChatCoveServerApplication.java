f20d72a7b71a5dee63166972b145209314778db7
oriental1212 <oriental011212@gmail.com>
2023-06-24 21:11:00 +0800

    系统初始化构建

---
+++ src/main/java/cool/oriental/chatcove/ChatCoveServerApplication.java
@@ -0,0 +1,12 @@
+package cool.oriental.chatcove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCoveServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatCoveServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cool/oriental/chatcove/ChatCoveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.oriental.chatcove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatCoveServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
